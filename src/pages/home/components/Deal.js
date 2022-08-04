7abde23035944eae6da2d96cc2dd6f3b390faccd
Israel Omitiran <omitiranisrael@gmail.com>
2022-08-04 14:13:07 +0100

    adding cards

--- src/pages/home/components/Deal.js
+++ src/pages/home/components/Deal.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../styles/Deal.scss'
+import left from '../../../assests/left_1.png'
+import right from '../../../assests/right_1.png'
 
 const Deal = () => {
   return (
     <div className='deal'>
-        <h6 className='d-h'>BY ZURI Team</h6>
-        <h3 className="h-bot">We cater for every level of <br/> expertise and need in the industry.</h3>
+        <div className="h-head">
+            <h6 className='d-h'>BY ZURI Team</h6>
+            <h3 className="h-bot">We cater for every level of <br/> expertise and need in the industry.</h3>
+        </div>
+        <div className="backimage">
+            <img src={left} alt="" className='left'/>
+            <img src={right} alt="" className='right'/>
+        </div>
     </div>
   )
 }
---
+++ src/pages/home/components/DealCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DealCard = () => {
+  return (
+    <div className='d-card'>
+        .d-topic
+    </div>
+  )
+}
+
+export default DealCard
